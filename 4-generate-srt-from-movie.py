afe200d59221f80ffe30e51df706f45b1b3585fe
masterRLX <kcusni@gmail.com>
2025-07-04 17:44:23 +0900

    Initial commit: Add scripts for extracting audio, transcribing, and generating subtitles

---
+++ 4-generate-srt-from-movie.py
@@ -0,0 +1,13 @@
+from moviepy import VideoFileClip
+from config import VIDEO_FILE, AUDIO_FILE
+
+def format_time():
+    pass
+
+def generate_srt_from_movie():
+
+    ##1. 영상 -> 오디오
+    ##2. 오디오 -> segments
+    ##3. segments -> srt 저장
+
+generate_srt_from_movie()
